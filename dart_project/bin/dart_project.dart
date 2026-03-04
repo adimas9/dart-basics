f93334fd89fab395067f2ad8f7ccb0f404d11ea3
adimas <satriaadimas641@gmail.com>
2026-03-04 11:45:50 +0700

    TELO

--- dart_project/bin/dart_project.dart
+++ dart_project/bin/dart_project.dart
@@ -1,6 +1,7 @@
 import 'package:dart_project/dart_project.dart' as dart_project;
 
 void main() {
-    var penulis = ‘EKRAKS’;
-    print(penulis);
+    var umur = 18;
+dynamic nama = ‘GALANG LUPA JALAN PULANG;
+String kota = ‘Jepara’;
 }
